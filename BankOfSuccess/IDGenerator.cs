530a74f2ca360c266fe551d1ee8dc3f9d181f50e
Ankita S <Ankita S@DESKTOP-9A8QKLN>
2024-03-20 15:01:54 +0530

    bank of success

--- BankOfSuccess/IDGenerator.cs
+++ BankOfSuccess/IDGenerator.cs
@@ -1,10 +1,20 @@
-﻿namespace BankOfSuccess
+﻿using System.IO;
+
+namespace BankOfSuccess
 {
-    class IDGenerator
+     class IDGenerator
     {
         public static int ID = 1000;
         public static int generateID()
         {
+            if(ID == 1000)
+            {
+                using (StreamReader reader = new StreamReader(@"getID.txt"))
+                {
+
+                    ID = int.Parse(reader.ReadToEnd());
+                }
+            }
             return ID++;
         }
     }
---
+++ BankOfSuccess/UI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOfSuccess
+{
+    public class UI
+    {
+        public static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+                int ID = IDGenerator.generateID();
+
+                Console.WriteLine(ID);
+            }
+        }
+
+    }
+}
